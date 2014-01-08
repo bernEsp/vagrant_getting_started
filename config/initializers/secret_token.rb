ca33fd87f20bbc767691d034a02636406e607cd9
Bernardo Galindo <bernardo466@gmail.com>
2014-01-08 02:05:02 -0600

    initial commit using vagrant and ruby 2.0 and rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VagrantGettingStarted::Application.config.secret_key_base = '7a28445655261d89c69126208f47cd579668520826b6e50d3df4d74f1ad8177a3ecc4f38c1f31f7b8a0972d56a6c46507ffc85cc65ca81689e787de5b3f5b54a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VagrantGettingStarted::Application.config.session_store :cookie_store, key: '_vagrant_getting_started_session'
